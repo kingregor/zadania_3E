d7fe4f4b92c93a42e6409e64b42a2fcf591f5e04
Grzegorz Kubiak <35864016+kingregor@users.noreply.github.com>
2024-10-07 14:34:34 +0200

    lekcja w C++

---
+++ lekcja 1.cpp
@@ -0,0 +1,51 @@
+/*
+zadania z C++
+*/
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World";
+    return 0;
+}
+
+//---------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
+
+//---------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string imie = "Grzegorz"; // operator przypisania
+    cout<<"Witaj "<<imie<<endl;
+    return 0;
+}
+
+//---------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string imie;
+	cout<<"Podaj swoje imię: ";
+	cin>>imie; // pobieranie danych
+    cout<<"Witaj "<<imie;
+    return 0;
+}
+
+
